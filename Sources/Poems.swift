077c1f660893a6fb6c53483abe16f4b95a087ac7
MarvinHeller <marvin.heller@deutschebahn.com>
2017-10-11 23:10:01 +0200

    add new poems

--- Sources/Poems.swift
+++ Sources/Poems.swift
@@ -3,13 +3,17 @@ import Foundation
 
 struct Poems {
     static let poems = [
-        "Nico ist ein dicker Wicht, drum widme ich ihm das Gedicht",
-        "Marvin, der diesen Bot hier schrieb, den haben alle Leute lieb",
-        "Malte, unser Superstar, den lieben wir, das ist doch klar",
-        "Anna haben alle lieb, sie ist gewiss kein Autodieb",
-        "Eva, unser neuer Agile Master entschärft jedes Scrumdisaster",
-        "Seebach, äy, der Grumpy Guy, braucht mal ein paar Wochen frei",
-        "Ben, dessen Haar ist stets gewellt, hat den Kasten längst schon kalt gestellt"
+        "Nico ist ein dicker Wicht, drum widme ich ihm das Gedicht.",
+        "Marvin, der diesen Bot hier schrieb, den haben alle Leute lieb.",
+        "Malte, unser Superstar, den lieben wir, das ist doch klar.",
+        "Anna haben alle lieb, sie ist gewiss kein Autodieb.",
+        "Eva, unser neuer Agility Master entschärft jedes Scrumdisaster.",
+        "Seebach, äy, der Grumpy Guy, braucht mal ein paar Wochen frei.",
+        "Ben, dessen Haar ist stets gewellt, hat den Kasten längst schon kalt gestellt.",
+        "Der Thomas oder TNT - Ein wahres Datenfeuerwerk, wie noch nie.",
+        "Baumeister DevBobs Bot-geplagt ist der, den man bei Unwissen am liebsten fragt.",
+        "Die Nancy, ach, so zart und fein, haut volle Kanne Agile rein.",
+        "Wer reitet so spät durch Nacht und Wind? Es ist der Timo, das Vorzeigekind. Er hat die Xbox wohl im Arm. Er fasst sie sicher, er hält sie warm."
     ]
     static func random() -> String {
         var randomNumber : Int
